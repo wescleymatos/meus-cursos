1ec6e865cf99c3145dcd5d3f5fdaa238acee92b2
Wescley Matos <wescleymatos@gmail.com>
2017-09-27 06:10:23 -0300

    configuração do template engine ejs

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
-var functions = require('firebase-functions');
+const functions = require('firebase-functions');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+  const person = {name: 'Wescley Matos'};
+  res.render('home/index', { person });
+});
+
+exports.app = functions.https.onRequest(app);
