20df4c439fe5a808bd9336b5302ec6119c497023
Wescley Matos <wescleymatos@gmail.com>
2017-09-27 07:46:50 -0300

    implementando inserção de dados no firebase database

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,12 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+
+const firebaseApp = firebase.initializeApp(
+  functions.config().firebase
+);
 
 const app = express();
 
@@ -11,10 +16,46 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// const authorized = (email, pass, redirect, error) => {
+//   firebaseApp
+//     .auth()
+//     .signInWithEmailAndPassword(email, pass)
+//     .then(() => 'deu certo')
+//     .catch((error) => error);
+// };
+
+const insertUser = (user) => {
+  const ref = firebaseApp.database().ref('users');
+
+  return ref.set(user)
+}
+
 app.get('/', (req, res) => {
   res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+
+  res.render('account/index');
+});
+
+app.post('/', (req, res) => {
+  const user = {
+    users: [{
+      email: req.bady.email,
+      pass: req.body.pass
+    }]
+  }
+
+  insertUser(user)
+    .then(() => res.redirect('/dashboard/index'))
+    .catch(() => res.redirect('/account/index'));
+
+});
+
+app.get('/dashboard', (req, res) => {
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+
   const person = {name: 'Wescley Matos'};
-  res.render('home/index', { person });
+
+  res.render('dashboard/index', { person });
 });
 
 exports.app = functions.https.onRequest(app);
